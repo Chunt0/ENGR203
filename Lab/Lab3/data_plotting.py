9aa7efe6d9ea9120c267112555c134c7cce361ca
Chunt0 <chunt.mail@gmail.com>
2023-04-30 13:57:05 -0700

    Updated

--- Lab/Lab3/data_plotting.py
+++ Lab/Lab3/data_plotting.py
@@ -1,3 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 def extract_data(data_string):
     data_lines = data_string.strip().split('\n')
     extracted_data = []
@@ -5,17 +8,20 @@ def extract_data(data_string):
     for line in data_lines:
         freq, value = line.split()
         freq = float(freq)
-        value = float(value.replace('dB', ''))
+        value = value.replace('dB', '').replace('(','').replace(')','')
+        value = value.split(",")
+        value = float(value[0])
         extracted_data.append((freq, value))
 
     return extracted_data
 
 
-filename = './ENGR203_lab3.txt'  # Replace with your actual filename
+filename = 'ENGR203_lab3.txt'  # Replace with your actual filename
 
-with open(filename, 'r') as file:
+with open(filename, 'r', encoding='utf-8') as file:
     data_string = file.read()
 
 extracted_data = extract_data(data_string)
 for freq, value in extracted_data:
     print(f"Frequency: {freq}, Value: {value}")
+
