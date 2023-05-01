9b2ae122ce38fdeb4ace56c0f3ff1e85f81bf715
Chunt0 <chunt.mail@gmail.com>
2023-05-01 15:09:18 -0700

    Updated

--- Lab/Lab3/test.py
+++ Lab/Lab3/test.py
@@ -2,9 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def transfer_func(s):
+    max_val = None
+    max_freq = None
+
     for i, val in enumerate(s):
-        s[i] = 20*np.log10(np.abs((val*200)/((val+10)*(val+2))))
-    return s
+        val_db = 20 * np.log10(np.abs((val * 200) / ((val + 10) * (val + 2))))
+
+        if max_val is None or val_db > max_val:
+            max_val = val_db
+            max_freq_index = i
+
+        s[i] = val_db
+
+    return s, max_val, max_freq_index
 
 def extract_data(data_string):
     data_lines = data_string.strip().split('\n')
@@ -18,8 +28,7 @@ def extract_data(data_string):
         x.append(freq)
         y.append(value)
 
-    return x,y
-
+    return x, y
 
 filename = 'ENGR203_lab3.txt'  # Replace with your actual filename
 
@@ -32,15 +41,16 @@ freq, v_out = extract_data(data_string)
 values = np.arange(0.01, 100000.01, 0.05).tolist()
 s = []
 for val in values:
-    val = 1j*val
+    val = 1j * val
     s.append(val)
 
-y = transfer_func(s)
-hertz = [val/(2*np.pi) for val in values]
+y, max_val, max_freq_index = transfer_func(s)
+hertz = [val / (2 * np.pi) for val in values]
 
+print("Transfer Function Bode Plot:")
+print("Max Vo: ", max_val)
+print("Frequency: ", hertz[max_freq_index])
 print()
-print()
-
 
 plt.semilogx(hertz, y)
 plt.grid(True)  # Show grid lines
@@ -49,10 +59,14 @@ plt.ylabel('Vo (dB)')
 plt.title('Transfer Function Bode Plot')
 plt.show()
 
+print("Constructed Circuit Bode Plot:")
+print("Max Vo: ", max(v_out))
+print("Frequency: ", freq[v_out.index(max(v_out))])
+print()
+
 plt.semilogx(freq, v_out)
 plt.grid(True)  # Show grid lines
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Vo (dB)')
 plt.title('Constructed Circuit Bode Plot')
 plt.show()
-
