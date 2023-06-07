443b87b81f712afc77a38af2c05736dc7c7cc8d6
Chunt0 <chunt.mail@gmail.com>
2023-06-07 08:22:01 -0700

    2023-06-07 08:22:01

--- Lab/Synth/LaTex/graph.py
+++ Lab/Synth/LaTex/graph.py
@@ -1,15 +1,35 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-# Data
-voltage = [0, 1, 2, 3, 4, 5]
-frequency = [18, 36.23, 74.41, 154.2, 321.0, 669.4]
+# Read data from each file
+for i in range(6):
+    filename = f"./data/CV{i}.txt"
+    x = []
+    y = []
+    with open(filename, 'r') as file:
+        for line in file:
+            values = line.strip().split()
+            x.append(float(values[0]))
+            y.append(float(values[1]))
+    # Shift x-axis values to start at zero
+    min_x = x[0]
+    x = [val - min_x for val in x]
 
-# Create the graph
-plt.scatter(voltage, frequency)
-plt.xlabel('CV Input Voltage (V)')
-plt.ylabel('Frequency (Hz)')
-plt.title('CV Input Voltage vs Frequency')
+    # Find the peaks and calculate the frequency
+    peaks, _ = np.where(np.diff(np.sign(np.diff(y))) < 0)
+    if len(peaks) >= 2:
+        peak_diff = x[peaks[1]] - x[peaks[0]]
+        frequency = 1 / peak_diff
+        print(f"CV{i} Frequency: {frequency:.2f} Hz")
+    else:
+        print(f"CV{i} Frequency: Not enough peaks to calculate")
 
-# Display the graph
-plt.show()
+    # Create a new figure and plot
+    plt.figure()
+    plt.plot(x, y)
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title(f'CV{i} Data')
 
+    # Display the plot for each data set
+    plt.show()
