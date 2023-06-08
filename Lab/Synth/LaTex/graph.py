3271826e49dd080211ddc0eaee8768cb19da0a1a
Chunt0 <chunt.mail@gmail.com>
2023-06-08 09:15:40 -0700

    2023-06-08 09:15:40

--- Lab/Synth/LaTex/graph.py
+++ Lab/Synth/LaTex/graph.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+freq = [18.5, 4854]
+
 # Read data from each file
-for i in range(6):
-    filename = f"./data/CV{i}.txt"
+for i in range(2):
+    filename = f"./data/CCV{i}.txt"
     x = []
     y = []
     with open(filename, 'r') as file:
@@ -15,21 +17,13 @@ for i in range(6):
     min_x = x[0]
     x = [val - min_x for val in x]
 
-    # Find the peaks and calculate the frequency
-    peaks, _ = np.where(np.diff(np.sign(np.diff(y))) < 0)
-    if len(peaks) >= 2:
-        peak_diff = x[peaks[1]] - x[peaks[0]]
-        frequency = 1 / peak_diff
-        print(f"CV{i} Frequency: {frequency:.2f} Hz")
-    else:
-        print(f"CV{i} Frequency: Not enough peaks to calculate")
 
     # Create a new figure and plot
     plt.figure()
     plt.plot(x, y)
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.title(f'CV{i} Data')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Voltage (v)')
+    plt.title(f'Voltage over Time at Node A: High Frequency - {freq[i]} Hz')
 
     # Display the plot for each data set
     plt.show()
